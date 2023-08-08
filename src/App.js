292335db4ae2cbdf18a3b2c9be4fbf3cbb19219b
adil-16 <adilmasood833@gmail.com>
2023-08-08 11:15:50 +0500

    [ADD] add v6 Routing

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
 import './App.css';
-import { Route, RouterProvider, Routes, createBrowserRouter } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import {  RouterProvider,createBrowserRouter } from 'react-router-dom';
 import { CartProvider } from './Context/CartContext'; 
 import Home from './pages/Home';
 import Products from './pages/Products';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Root from './pages/Root';
 
 ////backend main url nai rkhna ik key rkhni ha hr product ki
 const router = createBrowserRouter([
     {
       path: "/",
-      children: <Home/>
+      element: <Root/>,
+      children: [
+        {
+          path: "/",
+          element: <Home/>
+        },
+        {
+          path: "/products",
+          element: <Products/>
+    
+        },
+        {
+          path: "/about",
+          element: <About/>
+    
+        },
+        {
+          path: "/contact",
+          element: <Contact/>
+    
+        },
+      ]
 
-    },
-    {
-      path: "/products",
-      children: <Products/>
-
-    },
-    {
-      path: "/about",
-      children: <About/>
-
-    },
-    {
-      path: "/contact",
-      children: <Contact/>
-
-    },
-
-
+    }
 
 ]);
 
@@ -39,23 +42,9 @@ const router = createBrowserRouter([
 function App() {
 
   return (
-    // <CartProvider>
-    //   <Navbar />
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/products" element={<Products />} />
-    //     <Route path="/about" element={<About />} />
-    //     <Route path="/contact" element={<Contact />} />
-    //   </Routes>
-    //   <Footer />
-    // </CartProvider>
     <CartProvider>
-      <Navbar/>
       <RouterProvider router={router}/>
     </CartProvider>
-        
-     
-
     
   );
 }
